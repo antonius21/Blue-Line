5e213199b0d40411bc5d61a099b7ffe3ec3dcca2
Anton <45803223+antonius21@users.noreply.github.com>
2022-11-03 13:56:47 +0200

    Creating route classes

--- Blue Line/Model/Route.cs
+++ Blue Line/Model/Route.cs
@@ -8,5 +8,18 @@ namespace Blue_Line.Model
 {
     internal class Route
     {
+        private int _routeId;
+        private string _name;
+        private string _description;
+        private Station _stationDeparture;
+        private Station _stationArrival;
+        private Station[] stations;
+        private DateTime _departureTime;
+        private DateTime _ArravialTime;
+        private DateTime _routeTime;
+
+        public Route()
+        {
+        }
     }
 }
